2577aaf0e38f6b6162a3056eb099cd057acf4c5f
RIPHeisenberg <43754514+RIPHeisenberg@users.noreply.github.com>
2020-10-19 17:22:16 +0200

    Bluetooth als separates File Implementiert

---
+++ ble_app_uart_c/pca10100/s122/ses/BLE_Control.h
@@ -0,0 +1,28 @@
+
+#ifndef BLUETOOTH_H_
+#define BLUETOOTH_H_
+
+   
+    
+
+   /////////////////////////////////
+   //Bluetooth Initialization
+   /////////////////////////////////
+   static void log_init(void);
+   static void timer_init(void);
+   static void uart_init(void);
+   static void buttons_leds_init(void);
+   static void db_discovery_init(void);
+   static void power_management_init(void);
+   static void ble_stack_init(void);
+   void gatt_init(void);
+   static void nus_c_init(void);
+   static void scan_init(void);
+
+  //////////////////////////////////
+  //Bluetooth Functions
+  //////////////////////////////////
+  
+   
+
+ #endif //BLUETOOTH_H_
---
+++ ble_app_uart_c/pca10100/s122/ses/global.h
@@ -0,0 +1,42 @@
+
+
+
+#ifndef GLOBAL_H_
+#define GLOBAL_H_
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "nordic_common.h"
+#include "app_error.h"
+#include "app_uart.h"
+#include "ble_db_discovery.h"
+#include "app_timer.h"
+#include "app_util.h"
+#include "bsp_btn_ble.h"
+#include "ble.h"
+#include "ble_gap.h"
+#include "ble_hci.h"
+#include "nrf_sdh.h"
+#include "nrf_sdh_ble.h"
+#include "nrf_sdh_soc.h"
+#include "ble_nus_c.h"
+#include "nrf_ble_gatt.h"
+#include "nrf_pwr_mgmt.h"
+#include "nrf_ble_scan.h"
+
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_log_default_backends.h"
+
+
+
+
+////////////////////////////
+//Bluetooth Schnittstelle
+////////////////////////////
+extern void bluetoothINIT(void);
+extern void scan_start(void);
+
+
+#endif //GLOBAL_H_
